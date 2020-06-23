1c87b19b0406d75a9275959e7077ff0ca1726043
Chris Forsythe <chrisforsythe@chriss-mbp-2.home>
2020-06-23 20:53:12 +0100

    Started work on about page.

--- app.js
+++ app.js
@@ -68,3 +68,167 @@ carouselSlide.addEventListener('transitionend', () => {
         carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
 });
+
+
+
+.about-main-container {
+    margin-top: 0px;
+    display: grid;
+    grid-template-columns: 45% 55%;
+    grid-auto-rows: minmax(175px, auto);
+    column-gap: 10px;
+    overflow: hidden;
+}
+
+
+.about-image-box {
+    overflow: hidden;
+    height: 85%;
+}
+
+.badge-box {
+    align-items: center;
+    justify-content: center;
+}
+
+#badge {
+    height: 175px;
+    width: 175px;
+}
+
+.about-box1 {
+    width: 55%;
+    align-items: center;
+    margin: auto;
+}
+
+.about-box2 {
+    width: 60%;
+    position: relative;
+    left: 30%;
+}
+
+
+.video-box {
+    margin: auto;
+    background-color: cadetblue;
+
+
+}
+
+.video1 {
+    margin: 0 auto;
+    align-self: center;
+    width: 80%;
+}
+
+.about-main-container {
+    margin-top: 0px;
+    display: grid;
+    grid-template-columns: 45% 55%;
+    column-gap: 10px;
+    overflow: hidden;
+}
+
+
+.about-image-box {
+    overflow: hidden;
+    height: 85%;
+}
+
+
+
+#badge {
+    height: 175px;
+    width: 175px;
+}
+
+.about-box1 {
+    width: 55%;
+    align-items: center;
+    margin: auto;
+}
+
+.about-box2 {
+    width: 60%;
+    position: relative;
+    left: 30%;
+}
+
+
+.video-box {
+    margin: auto;
+    background-color: cadetblue;
+
+
+}
+
+.video1 {
+    margin: 0 auto;
+    align-self: center;
+    width: 80%;
+}
+
+.image-carousel-container {
+    position: relative;
+    border: 1.5px solid black;
+    overflow: hidden;
+    position: relative;
+    background-color: chartreuse;
+}
+
+.image-carousel {
+    display: flex;
+    width: 100%;
+    height: 430px;
+}
+
+.image-carousel img {
+    width: 100%;
+}
+
+#prevBtn {
+    position: absolute;
+    top: 50%;
+    z-index: 10;
+    left: 5%;
+    font-size: 30px;
+    color: white;
+    opacity: 0.9;
+    cursor: pointer;
+}
+
+#nextBtn {
+    position: absolute;
+    top: 50%;
+    z-index: 10;
+    right: 5%;
+    font-size: 30px;
+    color: white;
+    opacity: 0.9;
+    cursor: pointer;
+}
+
+#h2-about {
+    padding-bottom: 25px;
+}
+
+#p-about {
+    letter-spacing: 0.7px;
+    padding-bottom: 18px;
+    font-size: 14px;
+}
+
+#p-about1 {
+    letter-spacing: 0.7px;
+    font-size: 14px;
+}
+
+
+<div class="box video-box">
+    <iframe class="about-video" src="https://player.vimeo.com/video/180634920" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+  </div>
+
+ <div class="box about-box2">
+    <p>the Chinatown Soccer Club is one of the most inspirational soccer clubs around. It was founded in 2002, when adidas invited crews from New York's creative industry to take part in the Fanatic Tournament.</p>
+ </div>
